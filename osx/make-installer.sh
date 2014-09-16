a3fd6c2a7320a92f40cb73c3d42535b55cfbf4c3
Vadim Peretokin <vperetokin@gmail.com>
2014-09-17 06:39:05 +1000

    Fixed up the installer to include dynamically-loaded librariesw within it, and started working on a script to automate the installer creation. The script is nearly complete - it does require a pause to update Info.plist executable to be run_mudlet instead of Mudlet

--- osx/make-installer.sh
+++ osx/make-installer.sh
@@ -1,16 +1,45 @@
 #!/bin/bash
 
 # Retrieve latest source code without any history
-git clone --depth 1 https://github.com/Mudlet/Mudlet.git source
+git clone https://github.com/Mudlet/Mudlet.git source
 
 # In case it already exists, update it
 cd source/
 git pull
 
+# Switch to release banrch
+git fetch
+git checkout release_30
+
 # Setup PATH to find qmake
 PATH=~/Qt5.3.1/5.3/clang_64/bin:$PATH
 
-# Compile
 cd src/
+# Remove old Mudlet.app, as macdeployqt doesn't like re-doing things otherwise. Requires admin rights.
+sudo rm -rf Mudlet.app/
+
+# Compile
 qmake
 make -j 2
+
+# Bundle in Qt libraries
+./mac-deploy.sh
+
+# Bundle in dynamically loaded libraries
+sudo cp ../../lfs.so Mudlet.app/Contents/MacOS
+sudo cp ../../rex_pcre.so Mudlet.app/Contents/MacOS
+# rex_pcre has to be adjusted to load libcpre from the same location
+sudo cp ../../libpcre.1.dylib Mudlet.app/Contents/MacOS
+sudo cp -r ../../luasql Mudlet.app/Contents/MacOS
+
+# As well as the loader for them
+sudo cp ../../run_mudlet Mudlet.app/Contents/MacOS
+
+#Contents/MacOS:.:../MacOS:$DYLD_LIBRARY_PATH
+
+# Edit the executable to be run_mudlet instead of Mudlet
+read
+
+# Generate final .dmg
+rm ~/Desktop/Mudlet.dmg
+appdmg ./osx-installer/mudlet-appdmg.json ~/Desktop/Mudlet.dmg
