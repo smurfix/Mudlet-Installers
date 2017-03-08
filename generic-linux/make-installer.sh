0cef9d324c03574a68c94245ebd3804a3ab526fc
Vadim Peretokin <vperetokin@gmail.com>
2017-03-08 08:08:19 +0100

    Got Lua libraries to deploy, still need LuaGlobal to load
    Getting LuaGlobal to load the files it needs right will require modifications to LuaGlobal, possibly backported from the development branch as work has been done there to enable setting the search path from the C++ code.

--- generic-linux/make-installer.sh
+++ generic-linux/make-installer.sh
@@ -28,10 +28,33 @@ cd src/
 qmake
 make -j `nproc --all`
 
-# move the binary up to the root folder where there's a mudlet.desktop - linuxdeployqt likes them together
-cp mudlet ../
+# go up to the root folder and clean up the build/ folder
+cd ../../
+rm -rf build/
+mkdir build
 
-# go up to source
-cd ../
-../linuxdeployqt.AppImage ./mudlet -appimage -always-overwrite
+# delete previous appimage as well since we need to regenerate it twice
+rm Mudlet*.AppImage
 
+# move the binary up to the build folder
+cp source/src/mudlet build/
+# get mudlet-lua in there as well so linuxdeployqt bundles it
+cp -rf source/src/mudlet-lua build/
+# and the .desktop file so linuxdeployqt can pilfer it for info
+cp source/mudlet{.desktop,.png,.svg} build/
+
+# first go at generation
+echo "Generating AppImage for the first time"
+./linuxdeployqt.AppImage ./build/mudlet -appimage
+
+# now copy Lua modules we need in
+# this should be improved not to be hardcoded
+cp /usr/lib/x86_64-linux-gnu/lua/5.1/lfs.so      ./build/lib
+cp /usr/lib/x86_64-linux-gnu/lua/5.1/rex_pcre.so ./build/lib
+mkdir ./build/lib/luasql
+cp /usr/lib/x86_64-linux-gnu/lua/5.1/luasql/sqlite3.so ./build/lib/luasql
+cp /usr/lib/x86_64-linux-gnu/lua/5.1/zip.so      ./build/lib
+
+echo "Regenerating AppImage to include Lua C modules"
+# and regenerate (https://github.com/probonopd/linuxdeployqt/issues/67#issuecomment-279211530)
+./linuxdeployqt.AppImage ./build/mudlet -appimage
