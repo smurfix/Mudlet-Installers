a4bbc0f1e0c364b5d1089e9c8a67e328cf237cb0
Keneanung <kenenanung@googlemail.com>
2017-03-15 22:40:36 +0100

    Change the way the osx installer is created
    It is now possible to create a dmg (osx installer image) from a pre-compiled
    Mudlet.app. The script will also install all run time dependencies and copy them
    into the app bundle creating the dmg.
    
    Building and packaging mudlet is still possible by using the new
    build-and-make-installer.sh. This requires a "commit-ish" now to be more
    flexible about what will be built/packaged.
    
    I also put the appdmg files into this project because they are installer files.

---
+++ osx/build-and-make-installer.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# abort script if any command fails
+set -e
+
+# extract program name for message
+pgm=$(basename "$0")
+
+# check command line option for commit-ish that should be checked out
+commitish="$1"
+if [ -z "$commitish" ]; then
+  echo "Usage: $pgm <commit-ish>"
+  exit 2
+fi
+
+# Retrieve latest source code, keep history as it's useful to edit sometimes
+# if it's already there, keep it as is
+if [ ! -d "source" ]; then
+  git clone https://github.com/Mudlet/Mudlet.git source
+
+  # Switch to $commitish
+  (cd source && git checkout $commitish)
+fi
+
+# set the commit ID so the build can reference it later
+cd  source
+commit=$(git rev-parse --short HEAD)
+
+#install dependencies to compile mudlet
+CI/travis.osx.before_install.sh
+CI/travis.osx.install.sh
+
+# Setup PATH to find qmake
+PATH=/usr/local/opt/qt5/bin:$PATH
+
+# Add commit information to version
+cd src/
+perl -pi -e "s/BUILD = -dev.*$/BUILD = -dev-$commit/" src.pro
+cd ..
+
+mkdir build
+cd build/
+# Remove old Mudlet.app, as macdeployqt doesn't like re-doing things otherwise.
+rm -rf Mudlet.app/
+
+# Compile using all available cores
+qmake ../src/src.pro
+make -j `sysctl -n hw.ncpu`
+
+# now run the actual installer creation script
+cd ../..
+./make-installer.sh
--- osx/make-installer.sh
+++ osx/make-installer.sh
@@ -3,51 +3,51 @@
 # abort script if any command fails
 set -e
 
-# Retrieve latest source code, keep history as it's useful to edit sometimes
-if [ ! -d "source" ]; then
-  git clone https://github.com/Mudlet/Mudlet.git source
+# set path to find macdeployqt
+PATH=/usr/local/opt/qt5/bin:$PATH
+
+cd source/build
+
+# install installer dependencies
+brew update
+BREWS="sqlite3 lua@5.1 node wget"
+for i in $BREWS; do
+  brew outdated | grep -q $i && brew upgrade $i
+done
+for i in $BREWS; do
+  brew list | grep -q $i || brew install $i
+done
+if [ ! -f "macdeployqtfix.py" ]; then
+  wget https://raw.githubusercontent.com/aurelien-rainone/macdeployqtfix/master/macdeployqtfix.py
 fi
+echo "Need root permissions to install lua rocks..."
+sudo luarocks-5.1 install LuaFileSystem
+sudo luarocks-5.1 install lrexlib-pcre
+sudo luarocks-5.1 install LuaSQL-SQLite3 SQLITE_DIR=/usr/local/opt/sqlite
 
-# In case it already exists, update it
-cd source/
-git pull
-
-# Switch to release banrch
-git fetch
-git checkout release_30
-
-# Setup PATH to find qmake
-PATH=/Users/mudlet/Qt/5.8/clang_64/bin:$PATH
-
-cd src/
-# Remove old Mudlet.app, as macdeployqt doesn't like re-doing things otherwise. Requires admin rights.
-sudo rm -rf Mudlet.app/
-
-# Compile using all available cores
-qmake
-make -j `sysctl -n hw.ncpu`
+npm install -g appdmg
 
 # Bundle in Qt libraries
-cd ../../
-./mac-deploy.sh
+macdeployqt Mudlet.app
 
-cd source/src
+# fix unfinished deployment of macdeployqt
+python macdeployqtfix.py Mudlet.app/Contents/MacOS/Mudlet /usr/local/Cellar/qt5/5.8.0_1/
+
 # Bundle in dynamically loaded libraries
-sudo cp ../../lfs.so Mudlet.app/Contents/MacOS
-sudo cp ../../rex_pcre.so Mudlet.app/Contents/MacOS
+cp "/usr/local/lib/lua/5.1/lfs.so" Mudlet.app/Contents/MacOS
+cp "/usr/local/lib/lua/5.1/rex_pcre.so" Mudlet.app/Contents/MacOS
 # rex_pcre has to be adjusted to load libcpre from the same location
-sudo cp ../../libpcre.1.dylib Mudlet.app/Contents/MacOS
-sudo cp -r ../../luasql Mudlet.app/Contents/MacOS
+python macdeployqtfix.py Mudlet.app/Contents/MacOS/rex_pcre.so /usr/local/Cellar/qt5/5.8.0_1/
+cp -r "/usr/local/lib/lua/5.1/luasql" Mudlet.app/Contents/MacOS
 
-# As well as the loader for them
-sudo cp ../../run_mudlet Mudlet.app/Contents/MacOS
-
-# Edit the executable to be run_mudlet instead of Mudlet
-/usr/libexec/PlistBuddy -c "Set CFBundleExecutable run_mudlet" Mudlet.app/Contents/Info.plist
+# Edit some nice plist entries, don't fail if entries already exist
+/usr/libexec/PlistBuddy -c "Add CFBundleName string Mudlet" Mudlet.app/Contents/Info.plist || true
+/usr/libexec/PlistBuddy -c "Add CFBundleDisplayName string Mudlet" Mudlet.app/Contents/Info.plist || true
 
 # Generate final .dmg
+cd ../..
 rm -f ~/Desktop/Mudlet.dmg
 
 # If you don't get a background image on Sierra, either upgrade
 # or apply a workaround from https://github.com/LinusU/node-appdmg/issues/121
-appdmg ./osx-installer/mudlet-appdmg.json ~/Desktop/Mudlet.dmg
+appdmg appdmg/mudlet-appdmg.json ~/Desktop/Mudlet.dmg
