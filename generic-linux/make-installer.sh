d0b6e78c019914324d3b747675a47e58fc75255e
Vadim Peretokin <vperetokin@gmail.com>
2017-04-12 11:05:57 +0200

    Added pcre
    Addresses https://github.com/Mudlet/Mudlet/issues/872
    
    Started on sound support as well, but commented out for now - to test one issue at a time.

--- generic-linux/make-installer.sh
+++ generic-linux/make-installer.sh
@@ -90,19 +90,103 @@ echo "Generating AppImage for the first time"
 # this should be improved not to be hardcoded
 if [ "$(getconf LONG_BIT)" = "64" ]
     then
-    cp /usr/lib/x86_64-linux-gnu/lua/5.1/lfs.so      ./build/lib
-    cp /usr/lib/x86_64-linux-gnu/lua/5.1/rex_pcre.so ./build/lib
-    mkdir ./build/lib/luasql
-    cp /usr/lib/x86_64-linux-gnu/lua/5.1/luasql/sqlite3.so ./build/lib/luasql
-    cp /usr/lib/x86_64-linux-gnu/lua/5.1/zip.so      ./build/lib
+    cp /usr/lib/x86_64-linux-gnu/lua/5.1/lfs.so                            ./build/lib
+    cp /usr/lib/x86_64-linux-gnu/lua/5.1/rex_pcre.so                       ./build/lib
+    mkdir                                                                  ./build/lib/luasql
+    cp /usr/lib/x86_64-linux-gnu/lua/5.1/luasql/sqlite3.so                 ./build/lib/luasql
+    cp /usr/lib/x86_64-linux-gnu/lua/5.1/zip.so                            ./build/lib
+    # mp3 support for sounds 
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstflump3dec.so         ./build/lib
+    cp /lib/x86_64-linux-gnu/libz.so.1                                     ./build/lib
+    cp /lib/x86_64-linux-gnu/libpcre.so.3                                  ./build/lib
+     
+    # gstreamer bulk include. This needs to have the video stuff pruned... 
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstadder.so             ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstalsa.so              ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstapp.so               ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstaudioconvert.so      ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstaudiorate.so         ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstaudioresample.so     ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstaudiotestsrc.so      ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstcdparanoia.so        ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstcoreelements.so      ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstcoreelements.so      ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstcoreindexers.so      ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstdecodebin.so         ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstdecodebin2.so        ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstencodebin.so         ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstffmpegcolorspace.so  ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstflump3dec.so         ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstgdp.so               ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstgio.so               ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstlibvisual.so         ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstnice010.so           ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstogg.so               ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstpango.so             ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstplaybin.so           ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstplaybin.so           ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstpulse.so             ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstsubparse.so          ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgsttcp.so               ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgsttheora.so            ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgsttypefindfunctions.so ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstvideorate.so         ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstvideoscale.so        ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstvideotestsrc.so      ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstvolume.so            ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstvorbis.so            ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstximagesink.so        ./build/lib
+    # cp /usr/lib/x86_64-linux-gnu/gstreamer-0.10/libgstxvimagesink.so       ./build/lib
 else
-  cp /usr/lib/i386-linux-gnu/lua/5.1/lfs.so      ./build/lib
-  cp /usr/lib/i386-linux-gnu/lua/5.1/rex_pcre.so ./build/lib
-  mkdir ./build/lib/luasql
-  cp /usr/lib/i386-linux-gnu/lua/5.1/luasql/sqlite3.so ./build/lib/luasql
-  cp /usr/lib/i386-linux-gnu/lua/5.1/zip.so      ./build/lib
+  cp /usr/lib/i386-linux-gnu/lua/5.1/lfs.so                            ./build/lib
+  cp /usr/lib/i386-linux-gnu/lua/5.1/rex_pcre.so                       ./build/lib
+  mkdir                                                                ./build/lib/luasql
+  cp /usr/lib/i386-linux-gnu/lua/5.1/luasql/sqlite3.so                 ./build/lib/luasql
+  cp /usr/lib/i386-linux-gnu/lua/5.1/zip.so                            ./build/lib
+  # mp3 support for sounds 
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstflump3dec.so         ./build/lib
+  cp /lib/i386-linux-gnu/libz.so.1                                     ./build/lib
+  cp /lib/i386-linux-gnu/libpcre.so.3                                  ./build/lib
+   
+  # gstreamer bulk include. This needs to have the video stuff pruned. ..
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstadder.so             ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstalsa.so              ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstapp.so               ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstaudioconvert.so      ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstaudiorate.so         ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstaudioresample.so     ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstaudiotestsrc.so      ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstcdparanoia.so        ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstcoreelements.so      ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstcoreelements.so      ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstcoreindexers.so      ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstdecodebin.so         ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstdecodebin2.so        ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstencodebin.so         ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstffmpegcolorspace.so  ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstflump3dec.so         ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstgdp.so               ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstgio.so               ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstlibvisual.so         ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstnice010.so           ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstogg.so               ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstpango.so             ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstplaybin.so           ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstplaybin.so           ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstpulse.so             ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstsubparse.so          ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgsttcp.so               ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgsttheora.so            ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgsttypefindfunctions.so ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstvideorate.so         ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstvideoscale.so        ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstvideotestsrc.so      ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstvolume.so            ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstvorbis.so            ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstximagesink.so        ./build/lib
+  # cp /usr/lib/i386-linux-gnu/gstreamer-0.10/libgstxvimagesink.so       ./build/lib 
 fi
 
-echo "Regenerating AppImage to include Lua C modules"
+echo "Regenerating AppImage to include Lua C and GStreamer modules"
 # and regenerate (https://github.com/probonopd/linuxdeployqt/issues/67#issuecomment-279211530)
 ./linuxdeployqt.AppImage ./build/mudlet -appimage
